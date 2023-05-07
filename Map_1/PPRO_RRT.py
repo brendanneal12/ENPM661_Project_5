97409902d00b385856a15ae609d8e6ebdaa9d130
brendanneal12 <125323287+brendanneal12@users.noreply.github.com>
2023-05-07 17:01:30 -0400

    Map Update

--- Map_1/PPRO_RRT.py
+++ Map_1/PPRO_RRT.py
@@ -10,6 +10,7 @@ import math
 from matplotlib import pyplot as plt
 import random
 ##=====================================================Map Setup=============================================================##
+#Sets up arena with obstacles
 def setup(s, r):
 
     global arena
@@ -25,7 +26,7 @@ def setup(s, r):
         for y in range(0, 400):
         
             if checkClearance(x, y, s, r):
-                arena[399 - y, x] = darkGray
+                arena[y, x] = darkGray
     
     #Draw Obstacle Borders
     for x in range(0, 500):
@@ -33,7 +34,7 @@ def setup(s, r):
         for y in range(0, 400):
         
             if checkBorder(x, y, s):
-                arena[399 - y, x] = gray
+                arena[y, x] = gray
     
     #Draw Obstacles
     for x in range(0, 500):
@@ -41,21 +42,21 @@ def setup(s, r):
         for y in range(0, 400):
         
             if checkObstacle(x, y):
-                arena[399 - y, x] = white
+                arena[y, x] = white
                 
 #Checks to see if a point is within an obstacle
 def checkObstacle(x, y):
     
     #Left Squares
-    if x >= 50 and x < 125:
+    if x >= 50 and x < 100:
         
-        if (y >= 50 and y < 125) or (y >= 275 and y < 350):
+        if (y >= 50 and y < 100) or (y >= 300 and y < 350):
             return True
     
     #Right Squares
-    if x >= 375 and x < 450:
+    if x >= 400 and x < 450:
         
-        if (y >= 50 and y < 125) or (y >= 275 and y < 350):
+        if (y >= 50 and y < 100) or (y >= 300 and y < 350):
             return True
         
     #Left M Rectangle
@@ -94,15 +95,15 @@ def checkBorder(x, y, s):
     slopeHeight = int(round(s/math.cos(math.radians(64.29))))
     
     #Left Squares
-    if x >= 50 - s and x < 125 + s:
+    if x >= 50 - s and x < 100 + s:
         
-        if (y >= 50 - s and y < 125 + s) or (y >= 275 - s and y < 350 + s):
+        if (y >= 50 - s and y < 100 + s) or (y >= 300 - s and y < 350 + s):
             return True
     
     #Right Squares
-    if x >= 375 - s and x < 450 + s:
+    if x >= 400 - s and x < 450 + s:
         
-        if (y >= 50 - s and y < 125 + s) or (y >= 275 - s and y < 350 + s):
+        if (y >= 50 - s and y < 100 + s) or (y >= 300 - s and y < 350 + s):
             return True
         
     #Left M Rectangle
@@ -140,15 +141,15 @@ def checkClearance(x, y, s, r):
     slopeHeight = int(round((s + rr)/math.cos(math.radians(64.29))))
     
     #Left Squares
-    if x >= 50 - s - rr and x < 125 + s + rr:
+    if x >= 50 - s - rr and x < 100 + s + rr:
         
-        if (y >= 50 - s - rr and y < 125 + s + rr) or (y >= 275 - s - rr and y < 350 + s + rr):
+        if (y >= 50 - s - rr and y < 100 + s + rr) or (y >= 300 - s - rr and y < 350 + s + rr):
             return True
     
     #Right Squares
-    if x >= 375 - s - rr and x < 450 + s + rr:
+    if x >= 400 - s - rr and x < 450 + s + rr:
         
-        if (y >= 50 - s - rr and y < 125 + s + rr) or (y >= 275 - s - rr and y < 350 + s + rr):
+        if (y >= 50 - s - rr and y < 100 + s + rr) or (y >= 300 - s - rr and y < 350 + s + rr):
             return True
         
     #Left M Rectangle
@@ -178,19 +179,20 @@ def checkClearance(x, y, s, r):
 #Checks to see if a point is valid (by checking obstacle, border, and clearance, as well as making sure the point is within arena bounds)
 def checkValid(x, y, s, r):
     
-    if checkObstacle(x, 399 - y):
+    if checkObstacle(x, y):
         return False
     
-    if checkBorder(x, 399 - y, s):
+    if checkBorder(x, y, s):
         return False
     
-    if checkClearance(x, 399 - y, s, r):
+    if checkClearance(x, y, s, r):
         return False
     
     if (x < 0 or x >= 500 or y < 0 or y >= 400):
         return False
     
     return True
+
 
 ##=====================================Function and Class Definitions====================================================##
 class Node:
